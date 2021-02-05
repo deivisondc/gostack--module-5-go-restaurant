3378e575c5acaf304f3f82c579e10a3c1fb8ec7c
Deivison Cardoso <deivisondc@gmail.com>
2021-02-04 22:11:45 -0300

    add feature to add new food on menu

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -39,7 +39,9 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      api.post('/foods', { ...food, available: true }).then(response => {
+        setFoods([...foods, response.data]);
+      });
     } catch (err) {
       console.log(err);
     }
